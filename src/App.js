587217f3fee8eb84112d8e09fd408be2ddc53e89
elastra-fr <emmanuellastra@gmail.com>
2023-10-12 00:09:59 +0200

    composant footer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const Header =()=>{
 
 return(
 <>
-<header>
-<a><img src=''></img></a>
+<header id='mainHeader'>
+<img src={require("./images/logo.png")} alt='Logo Choco Pap'/>
 <ul>
 <li><a>Accueil</a></li> 
 <li><a>Boutique</a></li>
@@ -27,6 +27,42 @@ return(
 
 };
 
+const Footer=()=>{
+
+return(
+<footer id='mainFooter'>
+<div>
+  <h3>Choco Pap</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin facilisis odio at tincidunt. In hac habitasse platea dictumst. Class.
+
+</p>
+
+</div>
+
+<div>
+<h3>Contact</h3>
+<span>Adresse : 51, Rue du chocolat</span>
+<span>Téléphone : 01 23 45 67 89</span>
+<span>Horaires : 9h00-17h00 du Lundi au Vendredi</span>
+</div>
+
+<div>
+
+  
+</div>
+
+
+
+</footer>
+
+
+
+);
+
+
+
+}
+
 
 function App() {
   return (
@@ -35,7 +71,7 @@ function App() {
       <Header/>
 
 
-
+    <Footer/>
     </div>
   );
 }
