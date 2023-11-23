3380e145d217ac924f0e820072b4c70686f04a8b
Emmanuel LASTRA <emmanuellastra@gmail.com>
2023-11-23 17:38:07 +0100

    Ajoute titre + CTA fonctionnel sur carousel

--- src/components/NewCarousel.js
+++ src/components/NewCarousel.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import '../style/NewCarousel.css'
 import'../images/accueil1.jpg'
 import'../images/accueil2.jpg'
+import { Link } from 'react-router-dom';
 
 
 function NewCarousel({images}) {
@@ -49,6 +50,28 @@ return(
 <div className='rightArrow' onClick={slideRight}>&rsaquo;</div>*/}
 <div className='leftArrow' onClick={slideLeft}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></div>
 <div className='rightArrow' onClick={slideRight}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></div>
+
+
+<div className='carouselDots'>
+{images.map((_, index)=>{
+return(
+
+<div key={index} className={index==current ? "imgDot imgDotActive" : "imgDot"} onClick={()=>{setCurrent(index)}}></div>
+
+
+)
+
+
+
+})}
+
+
+
+</div>
+
+<div className='mainTitle'><h1>CHOCO PAP</h1></div>
+<Link className='mainButton' to='/Boutique'>VOIR LA BOUTIQUE</Link>
+
 
 </div>
     </div>
