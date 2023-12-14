4e80c99ccb46b9878f501509c79ab64be893f9b8
Emmanuel LASTRA <emmanuellastra@gmail.com>
2023-12-14 16:20:42 +0100

    Filtres responsive avec menu deroulant sur mobile

--- src/components/CheckBoxWithLabel.js
+++ src/components/CheckBoxWithLabel.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CheckBoxWithLabel({title, value, isChecked}) {
   return (
-    <div>
+    <div className='checkWithLabel'>
         <input type="checkbox" id={title} name={title} value={value}/>
   <label htmlFor={title}>{title}</label>
     </div>
--- src/components/Filtres.js
+++ src/components/Filtres.js
@@ -1,20 +1,57 @@
 import React from 'react'
 import '../style/Filtres.css'
 import CheckBoxWithLabel from './CheckBoxWithLabel'
+import { useState } from 'react'
+
+
+
 
 function Filtres() {
 
 const categories=[{titre : "Tous", value:"all"}, {titre : "Chocolat Blanc", value:"whiteChoc"}, {titre : "Chocolat au lait", value:"milkChoc"},{titre : "Chocolat noir", value:"blackChoc"},{titre : "Noix/Noisette", value:"nutChoc"},{titre : "Fruit", value:"fruit"},{titre : "Caramel", value:"caramel"},{titre : "Liqueur", value: "liqueur"},];
+const [showCat, setShowCat]= useState(false);
+const [showPrice, setShowPrice]= useState(false);
+const [showNotes, setShowNotes]= useState(false);
 
 
+const handleFilters=event=>{
+
+  console.log(event.currentTarget.id);
+
+switch(event.currentTarget.id)
+{
+case "catPlus":
+  setShowCat(!showCat);
+break;
+
+case "prixPlus":
+  setShowPrice(!showPrice);
+break;
+
+case "notesPlus":
+  setShowNotes(!showNotes);
+break;
+
+default:
+
+}
+
+
+
+
+
+
+
+
+}
 
   return (
     <>
 <div id="filters">
 <h3>FILTRES</h3>
 
-<div id='filtreCategories' className='filtreCont'>
-  <h4>Catégories</h4>
+<div id='filtreCategories' className={`filtreCont, ${showCat? "showCat" : "hideCat"}`}>
+  <h4>Catégories <span id="catPlus" onClick={handleFilters}><svg fill="white" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></span></h4>
 
   {categories.map((item, index)=>{
 
@@ -35,9 +72,34 @@ return(
 
 </div>
 
-<div id='filtrePrix' className='filtreCont'> 
-<h4>Prix</h4>
+<div id='filtrePrix' className={`filtreCont, ${showPrice? "showPrice" : "hidePrice"}`}> 
+<h4>Prix <span id="prixPlus" onClick={handleFilters}><svg fill="white" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></span></h4>
 
+<div>
+<label for="prixMin">Prix min:</label>
+<select name="min" id="prixMin">
+  <option value="1">1 €</option>
+  <option value="5">5 €</option>
+  <option value="10">10 €</option>
+  <option value="15">15 €</option>
+  <option value="30">30 €</option>
+  <option value="50">50 €</option>
+  <option value="75">75 €</option>
+ </select>
+ </div>
+
+<div>
+<label for="prixMax">Prix max:</label>
+<select name="max" id="prixMax">
+<option value="5">5 €</option>
+  <option value="10">10 €</option>
+  <option value="15">15 €</option>
+  <option value="30">30 €</option>
+  <option value="50">50 €</option>
+  <option value="75">75 €</option>
+  <option value="100" selected>100 €</option>
+</select>
+</div>
 
 
 
@@ -45,8 +107,34 @@ return(
 
 </div>
 
-<div className='filtreCont'>
-<h4>Notes</h4>
+<div className={`filtreCont, ${showNotes? "showNotes" : "hideNotes"}`}>
+<h4>Notes <span id="notesPlus" onClick={handleFilters}><svg fill="white" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></span></h4>
+
+<div>
+<label for="noteMin">Note min:</label>
+<select name="min" id="NoteMin">
+  <option value="1">1 €</option>
+  <option value="5">5 €</option>
+  <option value="10">10 €</option>
+  <option value="15">15 €</option>
+  <option value="30">30 €</option>
+  <option value="50">50 €</option>
+  <option value="75">75 €</option>
+ </select>
+ </div>
+
+<div>
+<label for="prixMax">Prix max:</label>
+<select name="max" id="prixMax">
+<option value="5">5 €</option>
+  <option value="10">10 €</option>
+  <option value="15">15 €</option>
+  <option value="30">30 €</option>
+  <option value="50">50 €</option>
+  <option value="75">75 €</option>
+  <option value="100" selected>100 €</option>
+</select>
+</div>
 
 
 </div>
